f715a13ebb66422cd3853178e4cfa42ea852267b
SalmanAli09 <msalmanali7890@gmail.com>
2022-05-10 18:57:21 +0500

    Js Assignment

---
+++ Chap # 1/app.js
@@ -0,0 +1,28 @@
+// ======> CHAPTER # 1 <======
+
+// ======> TASK # 1 <======
+
+// alert("Hello World")
+
+//  ======> TASK # 2 <======
+
+  // alert("Error!PLease enter a valid password.")
+
+// ======>  TASK # 3 <======
+
+// alert("Welcome to JS land ... \n Happy Coding!")
+
+// ======> TASK # 4 <======
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+//  ======> TASK # 5 <=======
+
+  // console.log(alert("Hello.. I can run JS through my web browser's console"));
+
+
+// ======>  TASK # 6 <======
+   
+// Have done in saperate file
+    //  ========================END======================
---
+++ Chap # 2/app.js
@@ -0,0 +1,61 @@
+//  ======> CHAPTER # 2  <======
+//  ======> TASK # 1 <======
+
+// var username;
+  
+//  ======> TASK # 2 <=====
+
+// var myName;
+// myName = "Salman Ali";
+
+//  ======> TASK # 3 <=====
+
+// var Message;
+// message = "Hello World !";
+// alert("HelloWorld");
+
+
+//  ======> TASK # 4 <=====
+
+
+// var name = ("Jhon Deo");
+// alert(name)
+// var age  = ("15 Years OLd");
+// alert("age")
+// var qulification  = (" Certified Mobile Application Development");
+// alert("qulification")
+
+
+
+//  ======> TASK # 5 <=====
+
+
+// var name = ("pizza");
+// alert("pizza\npizz\npiz\npi\np")
+
+//  ======> TASK # 6 <=====
+
+
+// var email =  "example@example.com";
+// alert("My E-Mail Address is " + email)
+
+//  ======> TASK # 7 <=====
+
+//  var book;
+//  book = ("A smarter way to learn JavaScrip");
+//  alert("I am trying to learn from the book" + book);
+  
+
+//  ======> TASK # 8 <=====
+
+
+//  var a = ("write using document.write ");
+//  document.write("Yah! I can write HTML content using Java script");
+
+
+//  ======> TASK # 9 <=====
+
+//  var style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//  alert(style);
+
+// ========================END======================
